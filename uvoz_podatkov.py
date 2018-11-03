6fa4becf591949767056055473db196533a74297
Tina Razic <tina.razic@gmail.com>
2018-11-03 20:25:19 +0100

    polepsan uvoz

--- uvoz_podatkov.py
+++ uvoz_podatkov.py
@@ -1,21 +1,20 @@
-import csv
-import json
-import os
 import re
-import sys
-import requests
 import orodja
 
 # URL glavne strani z nepremičninami
 # 'https://www.nepremicnine.net/oglasi-prodaja/ljubljana-mesto/stanovanje/'
 
+# mapa, v katero bomo shranili zajete strani
+# 'zajete-strani'
+
 # mapa, v katero bomo shranili podatke
-# obdelani-podatki'
+# 'obdelani-podatki'
 
 # ime CSV datoteke v katero bomo shranili podatke
 # 'stanovanja.csv'
 
-# najprej shranim vse strani dne 23.10.2018
+# najprej shranim vse spletne strani z oglasi
+# dne 23.10.2018, 59 strani
 # (od 1 do 59, vse skupaj 1768 zadetkov)
 
 for i in range(1, 60):
@@ -28,7 +27,7 @@ for i in range(1, 60):
         url, 'zajete_strani/stanovanja_ljubljana_{}.html'.format(i)
     )
 
-# sestavimo vzorec, ki poišče vse bloke oglasov
+# sestavimo regularani izraz, ki poišče vse bloke oglasov
 
 vzorec_bloka = re.compile(
     r'<div class="oglas_container'
@@ -36,7 +35,7 @@ vzorec_bloka = re.compile(
     r'<div class="clearer"></div>',
     re.DOTALL)
 
-# sestavimo vzorce za podatke, ki jih želimo
+# sestavim regularni izraz za podatke, ki jih želim pridobiti iz oglasa
 
 vzorec_oglasa = re.compile(
     r'id="(?P<id>\d+?)"'
@@ -57,10 +56,15 @@ vzorec_oglasa = re.compile(
     r'class="agencija">(?P<agencija>.*?)</span>',
     re.DOTALL)
 
+# sestavim regularni izraz, ki bo iz naslova oglasa ugotovil upravno enoto
+
 vzorec_enota = re.compile(
     r'class="title">(?P<enota>.*?)</span></a></h2>',
     flags=re.DOTALL
 )
+
+# sestavim regularni izraz, ki bo iz kratkega opisa ugotovil,
+# kdaj je bilo stanovanje adaptirano
 
 vzorec_adaptacija = re.compile(
     r'.*?'
@@ -68,38 +72,21 @@ vzorec_adaptacija = re.compile(
     flags=re.DOTALL
 )
 
-# poiščemo vse bloke oglasov na spletni strani
 
+# napišem fukncijo, ki bo izločila podatke iz bloka oglasa
 
-def stanovanja_na_strani(st_strani):
-    ime_datoteke = 'zajete_strani/stanovanja_ljubljana_{}.html'.format(
-        st_strani)
-    vsebina = orodja.vsebina_datoteke(ime_datoteke)
-    for blok in vzorec_bloka.findall(vsebina):
-        yield blok
-
-# shranimo vse bloke oglasov iz vseh strani v seznam oglasi
-
-oglasi = []
-for st_strani in range(1, 60):
-    for blok in stanovanja_na_strani(st_strani):
-        oglasi.append(blok)
-
-# v seznamu oglasi imamo sedaj vse bloke iz vseh strani
-
-# naredimo seznam slovarjev in uredimo podatke
-
-slovarji = []
-
-for blok in oglasi:
-    for ujemanje in vzorec_oglasa.finditer(blok):
+def izloci_podatke_oglasa(blok):
+    ''' Iz bloka oglasa izloči vse iskane podatke in jih spravi v slovar.'''
+    oglas = {}
+    ujemanje = vzorec_oglasa.search(blok)
+    if ujemanje is not None:
         oglas = ujemanje.groupdict()
 
         oglas['id'] = int(oglas['id'])
         oglas['tip'] = str(oglas['tip'])
         oglas['nadstropje'] = str(oglas['nadstropje'])
         oglas['leto'] = int(oglas['leto'])
-        oglas['opis'] = str(oglas['opis'])
+        oglas['opis'] = str(oglas['opis']).replace('&quot;', '')
         oglas['velikost'] = float(oglas['velikost'].replace(',', '.'))
         oglas['cena'] = float(oglas['cena'].replace('.', '').replace(',', '.'))
         oglas['agencija'] = str(oglas['agencija'])
@@ -118,20 +105,44 @@ for blok in oglasi:
         else:
             oglas['adaptirano'] = None
 
-        slovarji.append(oglas)
+    return oglas
+
+
+# napišem funkcijo, ki bo iz zajete spletne strani poiskala bloke
+# in na teh blokih uporabila funkcijo 'izloci_podatke_oglasa'
+
+def stanovanja_na_strani(st_strani):
+    ime_datoteke = 'zajete_strani/stanovanja_ljubljana_{}.html'.format(
+        st_strani)
+    vsebina = orodja.vsebina_datoteke(ime_datoteke)
+    for blok in vzorec_bloka.finditer(vsebina):
+        yield izloci_podatke_oglasa(blok.group(0))
+
+
+# napišem for zanko, ki uporabi funkcijo stanovanje_na_strani na vsaki
+# spletni strani in slovar s podatki oglasa doda v seznam oglasi
+
+oglasi = []
+for st_strani in range(1, 60):
+    for oglas in stanovanja_na_strani(st_strani):
+        if oglas != {}:
+            oglasi.append(oglas)
+
+# v seznamu oglasi imam sedaj vse slovarje, ki predstavljajo posamezni oglas
 
 # vsak slovar vsebuje ključe:
 # id, tip, nadstropje, leto, opis, velikost, cena, agencija, enota, adaptirano
 
-# zapišimo sedaj podatke v csv in razvrstimo stolpce
+# zapišem sedaj podatke v csv in razvrstim stolpce
+
 
 orodja.zapisi_csv(
-    slovarji,
+    oglasi,
     ['id', 'enota', 'tip', 'leto', 'adaptirano', 'nadstropje',
      'velikost', 'cena', 'agencija', 'opis'],
     'obdelani-podatki/stanovanja.csv'
 )
 
-# zapišemo še v json datoteki za pregled
+# zapišem še v json datoteki za pregled
 
-orodja.zapisi_json(slovarji, 'obdelani-podatki/stanovanja.json')
+orodja.zapisi_json(oglasi, 'obdelani-podatki/stanovanja.json')
